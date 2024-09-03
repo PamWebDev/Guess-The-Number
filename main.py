1ad42ec135c6e4b43d36e5c54c9ce144c286937a
Pamelajharely83 <pamelajharely83@gmail.com>
2024-09-03 12:05:34 -0500

    source code

---
+++ main.py
@@ -0,0 +1,15 @@
+""" importando módulo random para tener acceso a la función radint """
+import random
+
+randomNumber = random.randint(1,100)
+print(randomNumber)
+
+# Pseudocódigo base:
+# 1. Generar número random (respuesta ganadora)
+# 2. Usuaria ingrese un número (número de usuaria)
+# 3. Computadora generé un número random (número de sistema)
+# 3. Evaluar ambas respuestas (n. sistema y n. usuaria)
+# 4. Verficiar cual de las dos coincide con la respuesta ganadora
+# 5. Si el número de la usuaria coincide con la respuesta ganadora, terminar la partida
+# 6. Si el número del sistema coincide con la respuesta ganadora, terminar la partida
+
